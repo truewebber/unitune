f04656af38286978c890a400df73d55d24bc4c3e
Aleksey Kish <admin@truewebber.com>
2019-09-06 11:40:09 +0300

    + spotify token refresh

--- seeker/spotify.go
+++ seeker/spotify.go
@@ -2,8 +2,10 @@ package seeker
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/mgutz/logxi/v1"
 	"github.com/pkg/errors"
@@ -17,6 +19,8 @@ import (
 
 type (
 	spotify struct {
+		sync.Mutex
+		config *clientcredentials.Config
 		client sptf.Client
 	}
 )
@@ -32,23 +36,36 @@ func newSpotify() *spotify {
 		TokenURL:     sptf.TokenURL,
 	}
 
-	token, err := config.Token(context.Background())
+	s := &spotify{
+		config: config,
+	}
+
+	err := s.refreshSpotifyToken()
 	if err != nil {
 		log.Fatal("Error login Spotify", "error", err.Error())
 	}
 
-	client := sptf.Authenticator{}.NewClient(token)
-
-	return &spotify{
-		client: client,
-	}
+	return s
 }
 
 func (s *spotify) Seek(t tune.Tune) (*string, error) {
+	return s.seek(t, 0)
+}
+
+func (s *spotify) seek(t tune.Tune, retry int) (*string, error) {
 	query := fmt.Sprintf("%s - %s", t.Artist(), t.Track())
 
 	r, err := s.client.Search(query, sptf.SearchTypeTrack)
 	if err != nil {
+		spotifyErr, ok := err.(sptf.Error)
+		if ok && spotifyErr.Status == http.StatusUnauthorized && retry < 3 {
+			if err := s.refreshSpotifyToken(); err != nil {
+				log.Error("Error refresh Spotify token", "error", err.Error())
+			}
+
+			return s.seek(t, retry+1)
+		}
+
 		return nil, errors.Errorf("Error request Spotify response: %s, tune: %v", err.Error(), t)
 	}
 
@@ -75,4 +92,17 @@ func (s *spotify) Seek(t tune.Tune) (*string, error) {
 
 func (s *spotify) StreamerType() streamer.Type {
 	return streamer.TypeSpotify
+}
+
+func (s *spotify) refreshSpotifyToken() error {
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+
+	token, err := s.config.Token(context.Background())
+	if err != nil {
+		return err
+	}
+	s.client = sptf.Authenticator{}.NewClient(token)
+
+	return nil
 }
